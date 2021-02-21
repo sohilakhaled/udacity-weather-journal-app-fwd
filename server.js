1d0881168264b3b803b7f00772d0e3dfd9088c49
Sohila Khaled <sohila.khaled@orchtech.com>
2021-02-21 16:31:58 +0200

    start project

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+
+// Dependencies
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+//Add Post Route
+const data = [];
+app.post('/add', addInfo);
+
+function addInfo(req, res) {
+  projectData['date'] = req.body.date;
+  projectData['temp'] = req.body.temp;
+  projectData['userresponse'] = req.body.userresponse;
+  res.send(projectData);
+}
+
+// Callback function to complete GET '/all'
+app.get('/all', getInfo);
+
+function getInfo(req, res) {
+  res.send(projectData);
+}
+
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log(`running on localhost: ${port}`);
+};
--- website/app.js
+++ website/app.js
@@ -1,5 +1,9 @@
 /* Global Variables */
 
+// Base URL and API Key for OpenWeatherMap API
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '7dabfffe66086ea321b91f61b6205af5';
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
